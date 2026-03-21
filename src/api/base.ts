05605c9e27062b184ba62ee59f6d6005998f946f
misha-dobatkin <mdobatkin@servicepipe.ru>
2026-03-21 18:30:20 +0300

    changed baseurl

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 // const baseURL = import.meta.env.PROD ? '/api' : 'http://localhost:3001/api'
-const baseURL = import.meta.env.PROD ? 'http://217.177.74.174:3000/api' : 'http://localhost:3001/api'
+const baseURL = import.meta.env.PROD ? 'http://217.177.74.174:3001/api' : 'http://localhost:3001/api'
 const instance = axios.create({
 	baseURL,
 	withCredentials: true,
