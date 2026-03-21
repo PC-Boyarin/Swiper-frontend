fe022c82939226dbc3e8c8442a008bf06d58cead
misha-dobatkin <mdobatkin@servicepipe.ru>
2026-03-21 17:26:37 +0300

    restore vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    // Игнорировать ошибки при сборке
-    logOverride: { 'this-is-undefined-in-esm': 'silent' }
-  },
-  build: {
-    // Продолжать сборку даже при ошибках
-    rollupOptions: {
-      onwarn(warning, warn) {
-        // if (warning.code === 'ESLINT_ERROR') return;
-        warn(warning);
-      }
-    }
-  }
 })
